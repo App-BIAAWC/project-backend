500620a48ebc494bdad55dddd52779821c83e2a6
HylaSandra <93869226+HylaSandra@users.noreply.github.com>
2023-03-07 17:33:07 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__, template_folder="template")
+
+
+@app.route("/")
+def welcome():
+    return render_template("welcome.html")
+
+
+@app.route("/hello", methods=["GET", "POST"])
+def hello():
+    if request.method == "POST":
+        user_name = request.form.get("name")
+        return render_template("hello_user.html", user_name=user_name)
+    return render_template("hello.html")
+
+
+if __name__ == "__main__":
+    app.run()
