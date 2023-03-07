e5beaa725f76e75180f1ba291ea714a398b2bda4
HylaSandra <93869226+HylaSandra@users.noreply.github.com>
2023-03-07 18:02:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask import Flask, request, render_template
 app = Flask(__name__, template_folder="template")
 
 def print_python_version():
-    """Function printing python version."""
     print(sys.version)
 
     
