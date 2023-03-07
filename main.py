21f8405408fe6ede90b0d9dda26c5f8616744f18
HylaSandra <93869226+HylaSandra@users.noreply.github.com>
2023-03-07 18:15:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ app = Flask(__name__, template_folder="template")
 
 @app.route("/")
 def welcome():
+    """A single welcome to the user."""
     return render_template("welcome.html")
 
 
 @app.route("/hello", methods=["GET", "POST"])
 def hello():
+    """A personalized welcome to the user."""
     if request.method == "POST":
         user_name = request.form.get("name")
         return render_template("hello_user.html", user_name=user_name)
